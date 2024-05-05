2efcdb2d5c9aebb46fcb54ce5fd1a359109b8fd6
Eladi Sánchez <eladisanchez@gmail.com>
2024-05-05 21:37:54 +0200

    Claus redsys a env

--- config/redsys.php
+++ config/redsys.php
@@ -1,13 +1,13 @@
 <?php
 return [
 
-    'key'                   => 'VPQVWwX3oaLvrwfYF4kwqIBCe3lT2Hl/',
-    'key_test'              => 'sq7HjrUOBfKmC576ILgskD5srU870gJ7',
+    'key'                   => env('REDSYS_SECRET_KEY'),
+    'key_test'              => env('REDSYS_SECRET_KEY_TEST'),
     'url_notification'      => config('app.url').'/tpv_response',
     'url_ok'                => config('app.url').'/checkout/card',
     'url_ko'                => config('app.url').'/checkout/card-error',
 
-    'merchantCode'          => '346844715',
+    'merchantCode'          => env('REDSYS_MERCHANT_CODE'),
     'tradeName'             => 'Solsonès Entrades',
     'terminal'              => '1'
 
